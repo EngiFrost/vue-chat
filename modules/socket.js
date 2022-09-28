7c98dd4e7b5420678ba5ea912602fdd2c2909f33
EngiFrost <engifrost@gmail.com>
2022-09-28 13:26:56 +0300

    emitted socket io action from server!

--- modules/socket.js
+++ modules/socket.js
@@ -1,16 +1,27 @@
-import { Server } from 'socket.io'
-import {defineNuxtModule} from '@nuxt/kit'
+import { Server } from "socket.io";
+import { defineNuxtModule } from "@nuxt/kit";
 
 export default defineNuxtModule({
   setup(options, nuxt) {
-    nuxt.hook('listen', server => {
-      const io = new Server(server)
+    nuxt.hook("listen", (server) => {
+      const io = new Server(server, {
+        cors: {
+          origin: "http://localhost:3000",
+          methods: ["GET", "POST"],
+          transports: ["websocket"],
+          credentials: true,
+        },
+        allowEIO3: true,
+      });
 
-      nuxt.hook('close', () => io.close())
+      nuxt.hook("close", () => io.close());
 
-      io.on('connection', () => {
-        console.log("CONNECTION")
-      })
-    })
-  }
-})
+      io.on("connection", (socket) => {
+        console.log("CONNECTION");
+
+        socket.emit('getMessage', {text: 'WHAT'})
+        console.log ('action emitted')
+      });
+    });
+  },
+});
---
+++ store/index.js
@@ -0,0 +1,7 @@
+export const store = () => {};
+
+export const actions = {
+  SOCKET_getMessage: (ctx, data) => {
+    console.log(data);
+  },
+};
